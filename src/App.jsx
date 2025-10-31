023d69c5a1502bf2744be5a84cd87f5801a034a2
JayasriMalkari <jayasrim608@gmail.com>
2025-10-31 19:21:24 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ function App() {
         <ScrollToTop />
         <Suspense fallback={<LoadingFallback />}>
           <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
